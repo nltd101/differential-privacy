1e9f61b31cc63377f078fe2d3f98f529c38ed548
Ky <heovang097@gmail.com>
2021-07-10 22:59:37 +0700

    variance + mean

--- my_data_frame.py
+++ my_data_frame.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class myDataFrame(DataFrame):
-   
+
     def noise(self,colunm: str, method: str, epsilon: float) -> float:
         """return the noise based on method, colunm, epsilon
 
@@ -29,9 +29,9 @@ class myDataFrame(DataFrame):
         if (method not in Sensitivities[colunm].keys()):
             raise QueryException(NOT_SUPPORT_METHOD, colunm, method)
 
-            
+
         sensitivity = float(Sensitivities[colunm][method])
-        
+
         return np.random.laplace(0, sensitivity*1.0/epsilon)
 
     def mean(self):
@@ -66,22 +66,35 @@ class myDataFrame(DataFrame):
         Returns:
             float: statistic
             float: epsilon bubget if using full tablle
-        """        
+        """
         if (query_type == "mean"):
-            return self[colunm].mean()+self.noise(colunm, query_type, epsilon),epsilon
+            partial_epsilon = epsilon/2
+            noisy_sum = self[colunm].sum()+self.noise(colunm, "sum", partial_epsilon)
+            noisy_count = self[colunm].count()+self.noise(colunm, "count", partial_epsilon)
+            noisy_mean = noisy_sum/noisy_count
+            return noisy_mean, epsilon
         if (query_type == "max"):
-            return self[colunm].max()+self.noise(colunm, query_type, epsilon),epsilon
+            return self[colunm].max()+self.noise(colunm, "sum", epsilon),epsilon
         if (query_type == "min"):
-            return self[colunm].min()+self.noise(colunm, query_type, epsilon),epsilon
+            return self[colunm].min()+self.noise(colunm, "sum", epsilon),epsilon
         if (query_type == "median"):
-            return self[colunm].median()+self.noise(colunm, query_type, epsilon),epsilon
+            return self[colunm].median()+self.noise(colunm, "sum", epsilon),epsilon
         if (query_type == "mode"):
-            return self[colunm].mode()+self.noise(colunm, query_type, epsilon),epsilon
+            return self[colunm].mode()+self.noise(colunm, "sum", epsilon),epsilon
         if (query_type == "sum"):
             return self[colunm].sum()+self.noise(colunm, query_type, epsilon),epsilon
         if (query_type=="count"):
             return self[colunm].count()+self.noise(colunm, query_type, epsilon),epsilon
         if (query_type == "variance"):
-            return self[colunm].var()+self.noise(colunm,query_type,epsilon),epsilon
+            partial_epsilon = epsilon/3
+            square_sum = 0
+            for i in range(self[column].count()):
+                square_sum += self[column][i]
+            noisy_square_sum = square_sum + self.noise(colunm, "sum_square", partial_epsilon)
+            noisy_sum  = self[colunm].sum()+self.noise(colunm, "sum", partial_epsilon)
+            noisy_count = self[colunm].count()+self.noise(colunm, "count", partial_epsilon)
+            noisy_mean = noisy_sum/noisy_count
+            noisy_var = (1/noisy_count)*(noisy_square_sum - 2*noisy_mean*noisy_sum) + noisy_mean*noisy_mean
+            return noisy_var,epsilon
         else:
             raise QueryException(INVALID_QUERY, query_type)
--- sensitivities.py
+++ sensitivities.py
@@ -1,9 +1,9 @@
 Sensitivities ={
-    "age": {"sum": 90,"count":1},
+    "age": {"sum": 90,"count":1,"median":90,"sum_square":8100},
     "workclass":{"count":1},
-    "fnlwgt":{"sum": 1000000,"count":1}, 
-    "educational-num":{"sum": 15,"count":1},
-    "capital-gain": {"sum":100000,"count":1}, 
-    "capital-loss":{"sum": 316000,"count":1},
-    "hours-per-week":{"sum":124,"count":1}
+    "fnlwgt":{"sum": 1000000,"count":1,"median":1000000,"sum_square":1e10},
+    "educational-num":{"sum": 15,"count":1,"median":15,"sum_square":255},
+    "capital-gain": {"sum":100000,"count":1,"median":100000,"sum_square":1e10},
+    "capital-loss":{"sum": 316000,"count":1,"median":316000,"sum_square":1e11},
+    "hours-per-week":{"sum":124,"count":1,"median":124,"sum_square":15624}
 }
